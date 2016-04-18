282f405a282b68ffa4aea525e6a4185f73e8b8ee
asyncee <asyncee@users.noreply.github.com>
2016-04-18 19:41:48 +0300

    Refactored templates

--- src/chat/urls.py
+++ src/chat/urls.py
@@ -4,10 +4,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
-from . import views
+from chat_engine import views
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.homepage, name='homepage'),
+    url(r'^$', views.chat, name='chat'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- src/chat/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-
-def homepage(request):
-    return render(request, 'homepage.html')
